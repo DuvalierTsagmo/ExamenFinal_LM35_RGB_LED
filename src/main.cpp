c0f81fcfa7eb5a22820650b1bdcb8e4df52c286b
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-12-09 15:45:10 -0400

    rgb

--- src/main.cpp
+++ src/main.cpp
@@ -12,28 +12,28 @@ int bouton1 = 4;
 int bouton2 = 5;
 int Allume = 255;
 
-#define PIN_LED 2     // Control signal, connect to DI of the LED
-#define NUM_LED 1     // Number of LEDs in a strip
+#define PIN_LED 2 // Control signal, connect to DI of the LED
+#define NUM_LED 1 // Number of LEDs in a strip
 
 // Custom colour1: Yellow
-#define RED_VAL_1       255
-#define GREEN_VAL_1     255
-#define BLUE_VAL_1      0
+#define RED_VAL_1 255
+#define GREEN_VAL_1 255
+#define BLUE_VAL_1 0
 
 // Custom colour2: Purple
-#define RED_VAL_2       255
-#define GREEN_VAL_2     0
-#define BLUE_VAL_2      255
+#define RED_VAL_2 255
+#define GREEN_VAL_2 0
+#define BLUE_VAL_2 255
 
 // Custom colour3: Cyan
-#define RED_VAL_3       0
-#define GREEN_VAL_3     255
-#define BLUE_VAL_3      255
+#define RED_VAL_3 0
+#define GREEN_VAL_3 255
+#define BLUE_VAL_3 255
 
 // Custom colour4: White
-#define RED_VAL_4       255
-#define GREEN_VAL_4     255
-#define BLUE_VAL_4      255
+#define RED_VAL_4 255
+#define GREEN_VAL_4 255
+#define BLUE_VAL_4 255
 
 Adafruit_NeoPixel RGB_Strip = Adafruit_NeoPixel(NUM_LED, PIN_LED, NEO_GRB + NEO_KHZ800);
 
@@ -42,14 +42,14 @@ void setup()
     Serial.begin(9600);
     wifiConnect(); // Branchement au réseau WIFI
     MQTTConnect(); // Branchement au broker MQTT
-     RGB_Strip.begin();
-     RGB_Strip.show();
-     RGB_Strip.setBrightness(128); 
+    RGB_Strip.begin();
+    RGB_Strip.show();
+    RGB_Strip.setBrightness(128);
 }
 
 void loop()
 {
-  
+
     val = analogRead(A2); // Connect LM35 on Analog 0
     dat = (double)val * (5 / 10.24);
     Serial.print("Tep:"); // Display the temperature on Serial monitor
@@ -67,9 +67,8 @@ void loop()
         analogWrite(bouton2, 0);
         analogWrite(bouton1, Allume);
     }
+
     appendPayload("Tep", val);
     appendPayload("bouton1", Droit);
     sendPayload();
-
 }
-
