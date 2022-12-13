d9b363d10d0c4d0869b59e42d6d49ab072fd0019
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-12-13 11:24:14 -0400

    RGB 3 BOUTON

--- include/Secrets.h
+++ include/Secrets.h
@@ -3,55 +3,51 @@ SSID du WIFI ainsi que le mot de passe
 Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
 */
 
-//#define WIFI_SECRET_SSID "IDO2"
-//#define WIFI_SECRET_PASS "99Bidules!"
+// #define WIFI_SECRET_SSID "IDO2"
+// #define WIFI_SECRET_PASS "99Bidules!"
 
-//#define WIFI_SECRET_SSID "UNIFI_IDO2"
-//#define WIFI_SECRET_PASS "99Bidules!"
+// #define WIFI_SECRET_SSID "UNIFI_IDO2"
+// #define WIFI_SECRET_PASS "99Bidules!"
 
 #define WIFI_SECRET_SSID "IDO"
 #define WIFI_SECRET_PASS "99Bidules!"
 
+// #define WIFI_SECRET_SSID "RogersCmr"
+// #define WIFI_SECRET_PASS "CMR062021"
 
-//#define WIFI_SECRET_SSID "RogersCmr"
-//#define WIFI_SECRET_PASS "CMR062021"
+// #define WIFI_SECRET_SSID "HerveStella"
+// #define WIFI_SECRET_PASS "Maman123"
 
-
-//#define WIFI_SECRET_SSID "HerveStella"
-//#define WIFI_SECRET_PASS "Maman123"
-
-//#define WIFI_SECRET_SSID "FibreOP532"
-//#define WIFI_SECRET_PASS "9PXPE66PM6XM55M8"
-
+// #define WIFI_SECRET_SSID "FibreOP532"
+// #define WIFI_SECRET_PASS "9PXPE66PM6XM55M8"
 
 /*
 Information nécessaire pour le branchement a un Broker MQTT
 Dans le cadre du cours AIIA1013, ne modifié pas cet information
 */
-//#define SECRET_MQTT_SERVER_IP "192.168.2.99"
-//#define SECRET_MQTT_SERVER_IP "10.0.0.100"
-//#define SECRET_MQTT_SERVER_PORT 1883
+// #define SECRET_MQTT_SERVER_IP "192.168.2.99"
+// #define SECRET_MQTT_SERVER_IP "10.0.0.100"
+// #define SECRET_MQTT_SERVER_PORT 1883
 
-//#define SECRET_MQTT_SERVER_IP "192.168.2.50"
-//#define SECRET_MQTT_SERVER_PORT 1883
+// #define SECRET_MQTT_SERVER_IP "192.168.2.50"
+// #define SECRET_MQTT_SERVER_PORT 1883
 
-//#define SECRET_MQTT_SERVER_IP "192.168.2.50"
-//#define SECRET_MQTT_SERVER_PORT 1883
-
+// #define SECRET_MQTT_SERVER_IP "192.168.2.50"
+// #define SECRET_MQTT_SERVER_PORT 1883
 
 #define SECRET_MQTT_SERVER_IP "198.164.130.74"
 #define SECRET_MQTT_SERVER_PORT 1883
 
-//#define SECRET_MQTT_SERVER_IP "192.168.1.21"
-//#define SECRET_MQTT_SERVER_PORT 1883
+// #define SECRET_MQTT_SERVER_IP "192.168.1.21"
+// #define SECRET_MQTT_SERVER_PORT 1883
 
-//#define SECRET_MQTT_SERVER_IP "10.124.199.21"
-//#define SECRET_MQTT_SERVER_PORT 1883
+// #define SECRET_MQTT_SERVER_IP "10.124.199.21"
+// #define SECRET_MQTT_SERVER_PORT 1883
 
 /*
 Détails sur l'indentification de l'objet
 Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
 */
 
-#define SECRET_TOKEN "57bKY7ksIKHdEX42oJY5"
-#define SECRET_DEVICE_ID "d383e1e0-77f2-11ed-8fd7-e3d583114459"
+#define SECRET_TOKEN "zuixySEm9fD9eZzgpFVm"
+#define SECRET_DEVICE_ID "279cbff0-7aef-11ed-8fd7-e3d583114459"
